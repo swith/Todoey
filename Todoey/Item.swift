d8d1a402dc6e8df8f49a15e4f8d14c29574aef2b
Alexey Zvezdochkin <swith@tetrasis.ru>
2019-01-20 17:25:55 +0200

    persisting data using Realm

--- Todoey/Item.swift
+++ Todoey/Item.swift
@@ -12,6 +12,6 @@ import RealmSwift
 class Item: Object {
     @objc dynamic var title : String = ""
     @objc dynamic var done : Bool = false
-    @objc dynamic let dateCreated : Date = Date.init()
+    @objc dynamic var dateCreated : Date = Date()
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
